c775652704a1c2866ae2775b0dbcb09fc7f4cdf5
Mert <45696773+mertguven@users.noreply.github.com>
2020-09-30 00:12:57 +0300

    güncelleme

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:alaiye_travel/style.dart';
 
@@ -7,10 +9,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   TextEditingController textController = TextEditingController();
   String search = "";
-
+  String currentLanguage = "turkish";
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +33,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Spacer(flex: 20),
                     GestureDetector(
-                      onTap: (){
-                        print("turkey");
+                      onTap: () {
+                        setState(() {
+                          changeLanguage(currentLanguage, "turkish");
+                        });
                       },
                       child: Image.asset(
                         "assets/images/turkey.png",
@@ -43,8 +46,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Spacer(flex: 1),
                     GestureDetector(
-                      onTap: (){
-                        print("russia");
+                      onTap: () {
+                        setState(() {
+                          changeLanguage(currentLanguage, "russian");
+                        });
                       },
                       child: Image.asset(
                         "assets/images/russia.png",
@@ -54,8 +59,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Spacer(flex: 1),
                     GestureDetector(
-                      onTap: (){
-                        print("uk");
+                      onTap: () {
+                        setState(() {
+                          changeLanguage(currentLanguage, "english");
+                        });
                       },
                       child: Image.asset(
                         "assets/images/uk.png",
@@ -74,35 +81,203 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.grey.shade100,
                   borderRadius: BorderRadius.circular(25),
                 ),
-                child: Row(
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      search = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Ara",
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              //Activities phase
+              Expanded(
+                child: ListView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  scrollDirection: Axis.vertical,
                   children: [
-                    Icon(Icons.search),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Ara",
+                    Stack(
+                      alignment: Alignment.center,
+                      overflow: Overflow.visible,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          width: size.width,
+                          height: size.height * 0.3,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage("assets/images/stream.jpg"),
+                            ),
+                          ),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                            child: Container(
+                              color: Colors.black.withOpacity(0),
+                            ),
+                          ),
                         ),
-                        onChanged: (value){
-                          setState(() {
-                            search = value;
-                          });
-                        },
-                      ),
+                        Text(
+                          "Çay",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Poppins",
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      overflow: Overflow.visible,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          width: size.width,
+                          height: size.height * 0.3,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage("assets/images/mall.jpg"),
+                            ),
+                          ),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                            child: Container(
+                              color: Colors.black.withOpacity(0),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "AVM",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Poppins",
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      overflow: Overflow.visible,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          width: size.width,
+                          height: size.height * 0.3,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage("assets/images/boat_tour.jpg"),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "Tekne Turu",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Poppins",
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
                     ),
                   ],
                 ),
-              ),
-              ListView.builder(
-                itemBuilder: (context, index){
-                  return Card();
-                },
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  String listText(List<String> wallpaper) {
+    if (wallpaper[0] == "stream") {
+      return "Çay";
+    } else if (wallpaper[1] == "mall") {
+      return "Alışveriş Merkezi";
+    } else if (wallpaper[2] == "boat_tour") {
+      return "Tekne Turu";
+    }
+  }
+
+  Future changeLanguage(String currentLanguage, String targetLanguage) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text("${contentText(currentLanguage, targetLanguage)}"),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    currentLanguage = targetLanguage;
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text(
+                    "${actionsText(currentLanguage, targetLanguage).first}"),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                    "${actionsText(currentLanguage, targetLanguage).last}"),
+              ),
+            ],
+          );
+        });
+  }
+
+  contentText(String currentLanguage, String targetLanguage) {
+    if (currentLanguage == "turkish" && targetLanguage == "russian") {
+      return "Rusça diline çevirmek istediğinize emin misiniz?";
+    } else if (currentLanguage == "turkish" && targetLanguage == "english") {
+      return "İngilizce diline çevirmek istediğinize emin misiniz?";
+    } else if (currentLanguage == "english" && targetLanguage == "turkish") {
+      return "Are you sure you want to translate into Turkish?";
+    } else if (currentLanguage == "english" && targetLanguage == "russian") {
+      return "Are you sure you want to translate into Russian?";
+    } else if (currentLanguage == "russian" && targetLanguage == "turkish") {
+      return "Вы уверены, что хотите перевести это на турецкий?";
+    } else if (currentLanguage == "russian" && targetLanguage == "english") {
+      return "Вы уверены, что хотите перевести это на английский?";
+    }
+  }
+
+  List actionsText(String currentLanguage, String targetLanguage) {
+    var secim = new List(2);
+    switch (currentLanguage) {
+      case "turkish":
+        secim[0] = "Evet";
+        secim[1] = "Hayır";
+        break;
+      case "english":
+        secim[0] = "Yes";
+        secim[1] = "No";
+        break;
+      case "russian":
+        secim[0] = "да";
+        secim[1] = "нет";
+        break;
+    }
+    return secim;
   }
 }
